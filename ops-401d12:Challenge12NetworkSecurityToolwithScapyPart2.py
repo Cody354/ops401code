44a12a5b28efcc56ed4aef6cbebfd6152f4bb718
cody354 <cody.blahnik@gmail.com>
2024-05-15 09:57:42 -0500

    second

--- ops-401d12:Challenge12NetworkSecurityToolwithScapyPart2.py
+++ ops-401d12:Challenge12NetworkSecurityToolwithScapyPart2.py
@@ -11,35 +11,37 @@
 import sys
 import ipaddress
 from scapy.all import sr1, IP, ICMP, TCP
-#Define host IP
-ip = input("What IP would you like to use?")
-#Define port range or specific set of ports to scan
-ports = input("what port would you like to scan?")
-#Test each port in the specified range using a for loop
-for port in ports.split(","):
-    #Creating a packet
-    packet = IP(dst=ip) / TCP(dport=int(port), flags="S")
-    #Creating a response packet
-    response = sr1(packet, timeout=1, verbose=0)
 
-    #Checking inputs from users
-    if response:
-        #sending a ping to the ip on
-        if response.haslayer(TCP) and response[TCP].flags == 0x12:
-            print(f"Port {port}: Open")
-            #If flag 0x12 received, send a RST packet to graciously close the open connection. Notify the user the port is open.
-            rst_packet = IP(dst=ip) / TCP(dport=int(port), flags="R")
-            rst_response = sr1(rst_packet, timeout=1, verbose=0)
-            print(f"RST sent to close connection on port {port}")
-        #If flag 0x14 RST received, the port is closed
-        elif response.haslayer(TCP) and response[TCP].flags == 0x14:
-            print(f"Port {port}: Closed")
+def tcp_port_scan():
+    #Define host IP
+    ip = input("What IP would you like to use?")
+    #Define port range or specific set of ports to scan
+    ports = input("what port would you like to scan?")
+    #Test each port in the specified range using a for loop
+    for port in ports.split(","):
+        #Creating a packet
+        packet = IP(dst=ip) / TCP(dport=int(port), flags="S")
+        #Creating a response packet
+        response = sr1(packet, timeout=1, verbose=0)
+
+        #Checking inputs from users
+        if response:
+         #sending a ping to the ip on
+            if response.haslayer(TCP) and response[TCP].flags == 0x12:
+                print(f"Port {port}: Open")
+                #If flag 0x12 received, send a RST packet to graciously close the open connection. Notify the user the port is open.
+                rst_packet = IP(dst=ip) / TCP(dport=int(port), flags="R")
+                rst_response = sr1(rst_packet, timeout=1, verbose=0)
+                print(f"RST sent to close connection on port {port}")
+            #If flag 0x14 RST received, the port is closed
+            elif response.haslayer(TCP) and response[TCP].flags == 0x14:
+                print(f"Port {port}: Closed")
+            else:
+                #If no flag is received, notify the user the port is filtered and silently dropped
+                print(f"Port {port}: Filtered (Silently Dropped)")
         else:
-            #If no flag is received, notify the user the port is filtered and silently dropped
-            print(f"Port {port}: Filtered (Silently Dropped)")
-    else:
-        #If no response was received, the port is filtered
-        print(f"Port {port}: Filtered")
+            #If no response was received, the port is filtered
+            print(f"Port {port}: Filtered")
 # Pinging the CIDR bloc
 def icmp_ping_sweep():
     network = input("Enter the network address with CIDR block (e.g., 10.10.0.0/24): ")
