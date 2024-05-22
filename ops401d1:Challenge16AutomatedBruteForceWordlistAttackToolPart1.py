b55babadf3f8fbd8bcf1acc4326a19b2acfcbd68
cody354 <cody.blahnik@gmail.com>
2024-05-21 20:15:16 -0500

    second

--- ops401d1:Challenge16AutomatedBruteForceWordlistAttackToolPart1.py
+++ ops401d1:Challenge16AutomatedBruteForceWordlistAttackToolPart1.py
@@ -1,54 +1,43 @@
-
 #!/usr/bin/env python3
 
 # Script Name:                  ops-401d1:Challenge16AutomatedBruteForceWordlistAttackToolPart1.py
 # Author:                       Cody Blahnik
 # Date of latest revision:      5/20/24
-# Purpose:                      Brute force tool using nltk
+# Purpose:                      Brute force tool using wordlists
 
-import nltk
 import time
 
-def get_words():
-    nltk.download("words", quiet=True)
-    word_list = words.words()
-    return word_list
-# checking user input for ovffensive task
+# Function to perform offensive task
 def offensive():
     file_path = input("Enter the word list file path: ")
     delay = float(input("Enter the delay between words (in seconds): "))
     
     try:
-        with open(file_path,encoding = "ISO-8859-1") as file:
+        with open(file_path, encoding="ISO-8859-1") as file:
             for word in file:
                 word = word.strip()
                 print(word)
                 time.sleep(delay)
     except FileNotFoundError:
         print(f"File not found: {file_path}")
-# checking user input for deviseve 
+
+# Function to perform defensive task
 def defensive():
     user_string = input("Enter the string to search for: ")
     file_path = input("Enter the word list file path: ")
 
     try:
-<<<<<<< HEAD
-        with open(file_path, encoding = "ISO-8859-1") as file:
+        with open(file_path, encoding="ISO-8859-1") as file:
             word_list = [word.strip() for word in file]
             
-=======
-        with open(file_path, 'r') as file:
-            for word in file:
-            word_list = word.strip()
-            print(word_list)
->>>>>>> 4bbab40d37e81fb5f50e7b130040f6c56e833e58
         if user_string in word_list:
             print(f"The string '{user_string}' was found in the word list.")
         else:
             print(f"The string '{user_string}' was NOT found in the word list.")
     except FileNotFoundError:
         print(f"File not found: {file_path}")
-# the menu for the user
+
+# Main menu for user interaction
 def main():
     print("Automated Brute Force Wordlist")
     print("1. Offensive; Dictionary Iterator")
