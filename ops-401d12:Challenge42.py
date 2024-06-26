b77de6701ddb77b7dbf5cd0c3289f893c0cf6bbe
Cody Blahnik <153008650+Cody354@users.noreply.github.com>
2024-06-26 10:12:22 -0500

    Update ops-401d12:Challenge42.py

--- ops-401d12:Challenge42.py
+++ ops-401d12:Challenge42.py
@@ -2,38 +2,55 @@
 
 import nmap
 
+# Initialize the port scanner
 scanner = nmap.PortScanner()
 
 print("Nmap Automation Tool")
 print("--------------------")
 
+# Prompt the user to input the IP address to scan
 ip_addr = input("IP address to scan: ")
 print("The IP you entered is: ", ip_addr)
-type(ip_addr)
 
+# Prompt the user to select the type of scan to execute
 resp = input("""\nSelect scan to execute:
                 1) SYN ACK Scan
                 2) UDP Scan
-                3)              \n""") ### TODO: Select what your third scan type will be
+                3) Comprehensive Scan\n""")
 print("You have selected option: ", resp)
 
-range = '1-50'
+# Prompt the user to input the port range to scan
+port_range = input("Enter the port range you want to scan (e.g., 1-50): ")
 
-### TODO: Prompt the user to type in a port range for this tool to scan
-
+# Execute the scan based on user input
 if resp == '1':
+    # Perform a SYN ACK scan
     print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, range, '-v -sS')
+    scanner.scan(ip_addr, port_range, '-v -sS')
     print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
+    print("IP Status: ", scanner[ip_addr].state())
+    print("Protocols: ", scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp == '2':
-    ### TODO: Add missing code block here
-    print("Please enter a valid option") ### TODO: Remove this
+    # Perform a UDP scan
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sU')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+    print("Protocols: ", scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp == '3':
-    ### TODO: Add missing code block here
-    print("Please enter a valid option") ### TODO: Remove this
-elif resp >= '4':
+    # Perform a comprehensive scan
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sS -sU -sV -sC -A')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+    print("Protocols: ", scanner[ip_addr].all_protocols())
+    # Check for open TCP and UDP ports and print them
+    if 'tcp' in scanner[ip_addr].all_protocols():
+        print("Open TCP Ports: ", scanner[ip_addr]['tcp'].keys())
+    if 'udp' in scanner[ip_addr].all_protocols():
+        print("Open UDP Ports: ", scanner[ip_addr]['udp'].keys())
+else:
+    # Handle invalid options
     print("Please enter a valid option")
-``
